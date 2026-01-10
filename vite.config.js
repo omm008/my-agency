ee108f7b9ac03477b70654a785539a4bf7449a4a
Om Vishwakarma <vishwakarmaom003@gmail.com>
2026-01-10 11:20:04 +0530

    base add in vite

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ✅ THIS IS THE FIX
   plugins: [react()],
   build: {
-    // 1. Increase the warning limit (optional, to hide the noise)
     chunkSizeWarningLimit: 1000,
-
-    // 2. The Real Fix: Manual Chunking
     rollupOptions: {
       output: {
         manualChunks: {
-          // Split React and Router into a separate "vendor" chunk
           vendor: ["react", "react-dom", "react-router-dom"],
-          // Framer Motion is heavy, give it its own chunk
           framer: ["framer-motion"],
-          // Split icons if they are large
           icons: ["lucide-react"],
         },
       },
